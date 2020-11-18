2934dd636eeaeaace8a97ec205847633e002512a
Luis Calderon <luis.calderon@anahuac.mx>
2020-11-18 01:13:27 +0000

    First commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = new[] {12.7, 10.3, 6.11, 4.1};
+
+            double res = 0.0;
+
+            foreach(double number in numbers)
+            {
+                res += number;
+            }
+
+            System.Console.WriteLine(res);
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
