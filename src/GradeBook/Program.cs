e48b807abbb5c8f223477f8821c4163d49750314
Luis Calderon <luis.calderon@anahuac.mx>
2020-11-18 01:25:26 +0000

    Add gitignore and use of list

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -8,15 +8,17 @@ namespace GradeBook
         static void Main(string[] args)
         {
             double[] numbers = new[] {12.7, 10.3, 6.11, 4.1};
+            List<double> grades = new List<double>() {12.7, 10.3, 6.11, 4.1};
+            grades.Add(56.1);
 
             double res = 0.0;
-
-            foreach(double number in numbers)
+            foreach(double number in grades)
             {
                 res += number;
             }
 
             System.Console.WriteLine(res);
+            System.Console.WriteLine(res/grades.Capacity);
             
             Console.WriteLine("Hello World!");
         }
